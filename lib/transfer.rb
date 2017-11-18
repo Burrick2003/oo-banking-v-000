4456073f247ff3e8db88f64ebaa42bb972f37444
Blakeley Bond <burrick2003@yahoo.com>
2017-11-18 05:24:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ attr_accessor :sender, :receiver, :amount, :status
     if self.status == "pending" && self.valid?
       @sender.balance = @sender.balance - amount
       @receiver.balance = @receiver.balance + amount
-      self.status == "completed"
+      self.status == "complete"
 
     end
   end
