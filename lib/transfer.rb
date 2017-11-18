fd1159bb20b8a4fc16dc602e92436d7d8e8a6087
Blakeley Bond <burrick2003@yahoo.com>
2017-11-18 04:53:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,16 @@
 class Transfer
-attr_accessor :sender, :receiver, :amount
+attr_accessor :sender, :receiver, :amount, :status
   def initialize(send_name, rec_name, money)
     self.sender = send_name
     self.receiver = rec_name
     self.amount = money
+    self.status = pending
     @previous = 0
   end
 
+  def valid?
+
+  end
 
 
 
