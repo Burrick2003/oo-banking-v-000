4690a1699e075acd99f863225923fe9122b648f5
Blakeley Bond <burrick2003@yahoo.com>
2017-11-18 05:36:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,8 +14,8 @@ attr_accessor :sender, :receiver, :amount, :status
 
   def execute_transaction
     if @status == "pending" && self.valid?
-      @sender.balance = @sender.balance - amount
-      @receiver.balance = @receiver.balance + amount
+      @sender.balance = @sender.balance - @amount
+      @receiver.balance = @receiver.balance + @amount
       @status = "complete"
     elsif @sender.balance < self.amount
       @status = "rejected"
