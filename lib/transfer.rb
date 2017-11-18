9357f129a400df248486e1c75d7843b5a0a6e90f
Blakeley Bond <burrick2003@yahoo.com>
2017-11-18 05:28:27 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ attr_accessor :sender, :receiver, :amount, :status
       @sender.balance = @sender.balance - amount
       @receiver.balance = @receiver.balance + amount
       @status = "complete"
-    elsif
+
     end
   end
 
